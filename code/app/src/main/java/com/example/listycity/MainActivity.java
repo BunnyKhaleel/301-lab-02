a00b19f85647e6d5fd91b68dad8eb1a633c80a74
Khaleel <khaleelandfriend@gmail.com>
2026-01-16 12:41:15 -0700

    done pariticipation activity

--- code/app/src/main/java/com/example/listycity/MainActivity.java
+++ code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
 
+    int selected_city = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,14 +35,59 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        //buttons and textbox
+        Button actual_add = findViewById(R.id.actual_add_button);
+        EditText city_add = findViewById(R.id.city_name_textbox);
+        Button add = findViewById(R.id.add_button);
+        Button delete = findViewById(R.id.delete_button);
         cityList = findViewById(R.id.city_list);
 
-        String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
-
+        //list init
+        String []cities = {"Edmonton", "Vancouver", "Moscow"};
         dataList = new ArrayList<>();
         dataList.addAll(Arrays.asList(cities));
-
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
+
+        //unlokc add stuff
+        actual_add.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                city_add.setEnabled(true);
+                add.setEnabled(true);
+            }
+        });
+
+        //add button logic
+        add.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //get typed city
+                String city_typed = city_add.getText().toString();
+                dataList.add(city_typed);
+                cityAdapter.notifyDataSetChanged();
+                city_add.setText("");
+                city_add.setEnabled(false);
+                add.setEnabled(false);
+            }
+        });
+
+        //select city
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selected_city = position;
+            }
+        });
+
+        //remove
+        delete.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (selected_city != -1) {
+                    dataList.remove(selected_city);
+                    cityAdapter.notifyDataSetChanged();
+                    selected_city = -1;
+                }
+            }
+        });
+
     }
 }
